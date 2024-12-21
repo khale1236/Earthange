1a057d3421b1bc6101c8a99bbebf3de654fb1dee
le khiem <lekhiem@les-MacBook-Pro.lan>
2024-12-21 14:51:20 -0600

    create pages and rountes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Offers from './pages/Offers';
+import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <Router>
+      <Routes> {/* Opening tag */}
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/offers" element={<Offers />} />
+      </Routes> {/* Closing tag - This was the missing part! */}
+    </Router>
   );
 }
 
